b17819416523990236a821c9e1f18f1108bc5dd6
KALLEB <54485321+kallebdias@users.noreply.github.com>
2023-07-15 03:10:30 -0300

    Create matrix_effect.bat

---
+++ arquivos/matrix_effect.bat
@@ -0,0 +1,8 @@
+@echo off
+title Hacker v. 2.5.0
+color 0C
+mode 20, 10
+
+:matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%­­%random%%random%%random%%random%%random­%­%random%%random%%random%%random%%rando­m%­%random%%random%%random%%random%%rand­om%­%random%%random%
+goto matrix
